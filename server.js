42b235cad7934587019914a9202fbebfe92dbca8
Simi Sebastian <simiesebastian@seqato.com>
2024-07-04 11:38:21 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'admin',
+  host: 'localhost',
+  database: 'budgetTracker',
+  password: 'seq@123',
+  port: 5432,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error('Error connecting to PostgreSQL:', err);
+  } else {
+    console.log('Connected to PostgreSQL');
+  }
+});
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
